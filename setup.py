a58f30a330a3346d6a72536a46f960557e2bcf5c
Dmitry Azhichakov <dazhichakov@gmail.com>
2022-03-30 09:29:31 +0300

    Move sources to src/

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/diseaz/dsapy",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('src'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
